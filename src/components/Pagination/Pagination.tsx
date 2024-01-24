4c15e27ab6867ec6c45987ed39f9550b4a4b45f4
parwarr <parwar.habasch@gmx.de>
2024-01-24 20:05:00 +0100

    added styling to pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -21,16 +21,27 @@ const Pagination = () => {
   const startIndex = currentPage * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const subset = data.slice(startIndex, endIndex);
-  console.log(subset);
 
   const handlePageChange = selectedPage => {
     setCurrentPage(selectedPage.selected);
   };
 
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <Card cards={subset} />
-      <ReactPaginate pageCount={totalPages} onPageChange={handlePageChange} forcePage={currentPage} />
+      <ReactPaginate
+        pageCount={totalPages}
+        onPageChange={handlePageChange}
+        forcePage={currentPage}
+        containerClassName='flex gap-4 mt-4'
+        pageClassName='border rounded-xl px-3 py-2 cursor-pointer hover:bg-white hover:border hover:rounded-xl hover:px-3 hover:py-2 text-white'
+        breakClassName='border rounded-xl px-3 py-2 cursor-not-allowed hover:bg-white hover:border hover:rounded-xl hover:px-3 hover:py-2 text-white'
+        activeClassName='bg-gray-700 border rounded-xl px-3 py-2 cursor-pointer hover:bg-white hover:text-black hover:border hover:rounded-xl hover:px-3 hover:py-2 text-white'
+        previousLabel={'◀️'}
+        nextLabel={'▶️'}
+        previousClassName='border rounded-xl px-3 py-2 cursor-pointer hover:bg-white hover:border hover:rounded-xl hover:px-3 hover:py-2 text-white'
+        nextClassName='border rounded-xl px-3 py-2 cursor-pointer hover:bg-white hover:border hover:rounded-xl hover:px-3 hover:py-2 text-white'
+      />
     </div>
   );
 };
