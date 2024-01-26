640143e43e7508b4081abd6c32b9303ad6e26d89
parwarr <parwar.habasch@gmx.de>
2024-01-26 20:40:15 +0100

    feat: added type to state in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllCards } from '../../service/api/api.service';
+import { CardItemType } from '../../types/CardItem.type';
 import Navigation from '../Navigation/Navigation';
 import Searchbar from '../Searchbar/Searchbar';
 
 const Header = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<CardItemType[]>([]);
 
   useEffect(() => {
     getAllCards().then(response => {
@@ -15,7 +16,7 @@ const Header = () => {
 
   return (
     <header className='h-20 max-w-1100 bg-gradient-to-tr from-green-400 to-purple-500 rounded-md'>
-      <div className='container mx-auto flex items-center justify-between h-20'>
+      <div className='container mx-auto flex align-middle items-center justify-between h-20'>
         <Link to='/'>
           <img src='/test.gif' alt='logo' className='h-16 border-blue-400 border-2 rounded-full' />
         </Link>
